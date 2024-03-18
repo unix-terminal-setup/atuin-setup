961a50c5635556562eef6e8dac91dd5b3cba8dda
Coenraad Human <coen.human@gmail.com>
2024-03-18 15:17:19 +0200

    fix: some systems missing required dependencies for rust compile

--- setup.sh
+++ setup.sh
@@ -8,6 +8,9 @@ echo "This will setup Atuin for your machine locally"
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/linux-terminal-setup/rust-setup/main/setup.sh | bash
 
+# Required Dependencies
+sudo apt install pkg-config libssl-dev build-essential -y
+
 # Setup Atuin
 bash <(curl https://raw.githubusercontent.com/atuinsh/atuin/main/install.sh)
 atuin import auto

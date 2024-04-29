9b1dec3c33e896c52aefb7e06672343e21651ff8
Coenraad Human <coen.human@gmail.com>
2024-04-29 12:50:48 +0200

    feat: initial mac support

--- setup.sh
+++ setup.sh
@@ -8,8 +8,15 @@ echo "This will setup Atuin for your machine locally"
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/unix-terminal-setup/rust-setup/main/setup.sh | bash
 
-# Required Dependencies
-sudo apt install pkg-config libssl-dev build-essential -y
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	echo "Linux detected."
+        sudo apt install pkg-config libssl-dev build-essential -y
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    	echo "Mac detected."    
+else
+        echo "Unknown OS, abort."
+	exit 1
+fi
 
 # Setup Atuin
 bash <(curl https://raw.githubusercontent.com/atuinsh/atuin/main/install.sh)
@@ -17,6 +24,12 @@ atuin import auto
 
 # Install Atuin
 cargo install atuin
+
+# Include bash_profile
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    	echo '[[ -f ~/.bash-preexec.sh ]] && source ~/.bash-preexec.sh' >> ~/.bash_profile
+	echo 'eval "$(atuin init bash)"' >> ~/.bash_profile
+fi
 
 # Restart Terminals
 echo "Please restart your terminals"
